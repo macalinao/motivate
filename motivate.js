b2c963b48b5c27d67bfbcabdbc8b553af6d29844
Ian Macalinao <me@ian.pw>
2015-01-06 21:11:34 -0600

    Fix seed not being set

--- motivate.js
+++ motivate.js
@@ -4,7 +4,7 @@ var quotes = require('./quotes.json');
 
 module.exports = function() {
   var seed = parseInt(moment().format('YYYYMMDD'));
-  var engine = R.engines.mt19937().seed();
+  var engine = R.engines.mt19937().seed(seed);
   var index = R.integer(0, quotes.length - 1)(engine);
   return quotes[index];
 };
